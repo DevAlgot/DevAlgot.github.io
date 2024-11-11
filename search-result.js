3b503389fe9d543e5b5e0d5f78105ea2469abeb7
Algot Grönvall <algot.gronvall@hotmail.com>
2024-11-11 18:49:40 +0100

    minor changes to responive css

--- search-result.js
+++ search-result.js
@@ -33,7 +33,7 @@ async function setUpMovies(movies) {
 
         const posterDiv = document.createElement('img');
         posterDiv.className = 'poster';
-        if (movie.poster_path == null) posterDiv.src = "E:/Algot/Documents/HTML/Movie Site/Images/img-not-found.png";
+        if (movie.poster_path == null) posterDiv.src = "../Images/image-not-found.png";
         else posterDiv.src = "https://image.tmdb.org/t/p/original/"+movie.poster_path;
 
 
